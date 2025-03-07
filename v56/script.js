d53043b80638deeaad7a9ec52b191c3b522f133d
Laiba Fatima <laibafatimasheikh@yahoo.com>
2025-03-07 15:48:52 +0500

    js

---
+++ v56/script.js
@@ -0,0 +1,10 @@
+console.log("Condition tutorials")
+let age =17
+let grace =2
+if((age+grace)>18)
+    {console.log("Adult")
+
+    }
+else{
+    console.log("Not adult")
+}
---
+++ v57/script.js
@@ -0,0 +1,23 @@
+console.log("Tutorial loops")
+let a=1
+for (let i = 0; i < 10; i++) { //basic for
+    console.log(i)
+    
+    
+}
+let obj ={
+    name: "laiba",
+    role: "Programmer",
+    company:"laiba world"
+}
+for (const key in obj) {//forin
+    
+       
+        console.log(key)
+        
+    
+}
+for (const c of "laiba") { //iteratorloop ''forof
+    console.log(c)
+    
+}
