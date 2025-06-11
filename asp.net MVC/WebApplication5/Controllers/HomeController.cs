c152108c14b6bcaafd36c6daa89baa071b9a240b
Laiba Fatima Sheikh <laibafatimashiekh@yahoo.com>
2025-06-11 17:39:56 +0500

    asp mvc

---
+++ asp.net MVC/WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Session["Message"] = "Hi session";
+
+            string[] fruits = { "Apple", "Banana", "Cherry" };
+            Session["fruitArray"] = fruits;
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
