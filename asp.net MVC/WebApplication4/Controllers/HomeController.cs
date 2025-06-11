95e086bce3a09fa0858e0af9045297842cef8a18
Laiba Fatima Sheikh <laibafatimashiekh@yahoo.com>
2025-06-11 17:44:47 +0500

    asp mvc

---
+++ asp.net MVC/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        // GET: Home
+        public ActionResult Index()
+
+        {
+            ViewData["message1"] = "laiba1 viewdata";
+            ViewBag.message2 = "laiba2 viewbag";
+            TempData["message3"] = "laiba3 tempdata";
+
+            //string[]fruits= { "apple", "banana", "orange" };
+            //TempData["FruitsArray"] = fruits;
+
+
+            //return RedirectToAction("About");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (TempData["message3"] != null)
+            {
+                TempData["message3"].ToString();
+            }
+            TempData.Keep();
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            if (TempData["message3"] != null)
+            {
+                TempData["message3"].ToString();
+            }
+            return View();
+        }
+    }
+}
