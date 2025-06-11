24edd76f5d41bcb802c2f5fb75238da397b234c9
Laiba Fatima Sheikh <laibafatimashiekh@yahoo.com>
2025-06-11 17:48:19 +0500

    asp mvc

---
+++ asp.net MVC/WebApplication16/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication16.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Session["username"] == null)
+            {
+            
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
---
+++ asp.net MVC/WebApplication16/Controllers/LoginController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication16.Models;
+
+namespace WebApplication16.Controllers
+{
+    public class LoginController : Controller
+    {
+        LoginDBEntities db= new LoginDBEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    
+        [HttpPost]
+        public ActionResult Index(user s)
+        {
+            if (ModelState.IsValid)
+            {
+              
+                var credentials=db.users.Where(model => model.username == s.username && model.password == s.password).FirstOrDefault();
+                if (credentials != null)
+                {
+                    Session["id"] = credentials.id.ToString();
+                    Session["username"] = credentials.username.ToString();
+                    Session["password"] = credentials.password.ToString();
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Invalid username or password";
+                    return View();
+                }
+
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(user s)
+        {
+            if (ModelState.IsValid)
+            {
+                db.users.Add(s);
+                int a =db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["SuccessMessage"] = "Registration Successful";
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    ViewBag.SuccessMessage = "Registration Failed";
+                }
+            }
+            return View();
+        }
+
+    }
+}
