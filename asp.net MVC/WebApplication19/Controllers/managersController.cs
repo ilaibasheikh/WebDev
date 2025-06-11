f1c362cc1c502765abcf746caae9b5875e297178
Laiba Fatima Sheikh <laibafatimashiekh@yahoo.com>
2025-06-11 17:49:14 +0500

    asp mvc

---
+++ asp.net MVC/WebApplication19/Controllers/managersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication19.Models;
+
+namespace WebApplication19.Controllers
+{
+    public class managersController : Controller
+    {
+        private EmployeeDBEntities db = new EmployeeDBEntities();
+
+        // GET: managers
+        public ActionResult Index()
+        {
+            return View(db.managers.ToList());
+        }
+
+        // GET: managers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            manager manager = db.managers.Find(id);
+            if (manager == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manager);
+        }
+
+        // GET: managers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: managers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,NAME,role")] manager manager)
+        {
+            if (ModelState.IsValid)
+            {
+                db.managers.Add(manager);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(manager);
+        }
+
+        // GET: managers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            manager manager = db.managers.Find(id);
+            if (manager == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manager);
+        }
+
+        // POST: managers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,NAME,role")] manager manager)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(manager).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(manager);
+        }
+
+        // GET: managers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            manager manager = db.managers.Find(id);
+            if (manager == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manager);
+        }
+
+        // POST: managers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            manager manager = db.managers.Find(id);
+            db.managers.Remove(manager);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
