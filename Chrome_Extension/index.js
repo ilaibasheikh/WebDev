990257c1e4df31081bd3537700d76f37b60667d1
Laiba Fatima Sheikh <laibafatimasheikh@yahoo.com>
2025-07-09 21:57:14 +0500

    chrom-extension

--- Chrome_Extension/index.js
+++ Chrome_Extension/index.js
@@ -1,4 +1,31 @@
-const reci ="laiba";
-const sender="fatima";
-const email = `hey ${reci}! how r u? Cheers ${sender}`
-console.log(email);
+let myName= 'laiba';
+
+function greeting(greeting) {
+    return greeting;
+}
+function greet(name) {
+    console.log(greeting('See u soon ') + name);
+
+}
+greet('laiba')
+
+function fullGreeting(greeting, name){
+    console.log(`${greeting}, ${name} booboo!`);
+    
+}
+fullGreeting('See u soon', 'laiba')
+
+function add(num1, num2){
+return num1+num2;
+}
+
+let key = add(5,6)
+console.log(key);
+
+let me =[1,2,3,4]
+
+function getFirst(arr){
+    return arr[0    ];
+}
+let output = getFirst(me)
+console.log(output);
--- Chrome_Extension/script.js
+++ Chrome_Extension/script.js
@@ -1,30 +1,40 @@
+let myLeads = []
+let oldLeads = []
 const inputbtn = document.getElementById('input-btn');
-let myLeads=[]
 const inputField = document.getElementById('input-el');
-const ulEl=document.getElementById('ul-el');
+const ulEl = document.getElementById('ul-el');
+const delbtn = document.getElementById('del-btn');
+const leadfromlocal = JSON.parse(localStorage.getItem("myLeads"))
 
-inputbtn.addEventListener('click', function(){    
+if (leadfromlocal) {
+    myLeads = leadfromlocal
+    render(myLeads)
+}
+
+inputbtn.addEventListener('click', function () {
     myLeads.push(inputField.value);
-   
-    renderLeads()
-    inputField.value=""
-    
+    inputField.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads) 
 })
-function renderLeads(){
 
-    let listItems=""
-    for (let index = 0; index < myLeads.length; index++) {
-        // listItems +="<li><a target='_blank' href=' " + myLeads[index]+ "'>" +myLeads[index] +"</a></li>"
-        listItems +=`
+delbtn.addEventListener('dblclick', function () {
+    localStorage.clear();
+    myLeads = []
+    render(myLeads)
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let index = 0; index < leads.length; index++) {
+        // listItems +="<li><a target='_blank' href=' " + leads[index]+ "'>" +leads[index] +"</a></li>"
+        listItems += `
         <li>
-            <a target='_blank' href='${myLeads[index]}'>${myLeads[index]}</a>
+            <a target='_blank' href='${leads[index]}'>${leads[index]}</a>
         </li>`;
-        
-        
         // const li = document.createElement('li');
-        // li.textContent = myLeads[index];
+        // li.textContent = leads[index];
         // ulEl.append(li)
-        
     }
-    ulEl.innerHTML=  listItems
+    ulEl.innerHTML = listItems
 }
