9fe788d1ac860afdcc4dd53248f41c76da1dee19
Laiba Fatima Sheikh <laibafatimasheikh@yahoo.com>
2025-07-10 12:51:26 +0500

    chrome-extension

--- Chrome_Extension/script.js
+++ Chrome_Extension/script.js
@@ -4,18 +4,33 @@ const inputbtn = document.getElementById('input-btn');
 const inputField = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
 const delbtn = document.getElementById('del-btn');
+const tabbtn = document.getElementById('tab-btn');
 const leadfromlocal = JSON.parse(localStorage.getItem("myLeads"))
+
 
 if (leadfromlocal) {
     myLeads = leadfromlocal
     render(myLeads)
 }
 
+tabbtn.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        //  let activeTab=tabs[0];
+        //  let activeTabId= activeTab.id;
+        // console.log(tabs);
+
+        // console.log(tabs[0].url);
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    });
+
+})
 inputbtn.addEventListener('click', function () {
     myLeads.push(inputField.value);
     inputField.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads) 
+    render(myLeads)
 })
 
 delbtn.addEventListener('dblclick', function () {
